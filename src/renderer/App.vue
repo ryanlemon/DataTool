3c2668eaa9dae220e10c151b7baf1c0b8285785b
Ryan Zou <ryanlemon@outlook.com>
2018-10-31 23:31:37 +0800

    use vue-router

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <div class="tabs">
+      <ul>
+        <li>
+          <router-link :to ="{name:'landing'}">
+            <div>购物车</div>
+          </router-link>
+        </li>
+        <li>
+          <router-link :to ="{name:'test'}">
+            <div>个人中心</div>
+          </router-link>
+        </li>
+      </ul>
+    </div>
+    <div class="content">
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -6,13 +6,22 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'landing-page',
+      path: '/landing',
+      name: 'landing',
       component: require('@/components/LandingPage').default
     },
     {
       path: '*',
       redirect: '/'
+    },
+    {
+      path: '/',
+      redirect: '/landing'
+    },
+    {
+      path: '/test',
+      name: 'test',
+      component: require('@/components/Test').default
     }
   ]
 })
