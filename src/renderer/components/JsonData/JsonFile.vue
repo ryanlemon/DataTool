2eef208e2f4cefc1bda8ff394cbc7313f255cb69
Ryan Zou <ryanlemon@outlook.com>
2018-11-08 00:53:09 +0800

    selec-options

--- src/renderer/components/JsonData/JsonFile.vue
+++ src/renderer/components/JsonData/JsonFile.vue
@@ -1,14 +1,15 @@
 <script>
 import fs from 'fs-extra'
 const dataPath = 'D:\\Develop\\VueProj\\ScreenData'
-
+const list = fs.readdir(dataPath + '\\sd-screen\\')
 export default {
   getFileList: function (type) {
     if (type === 'screen') {
-      fs.readdir(dataPath + '\\sd-screen', (err, files) => {
-        if (!err) { return files } else { err.toString() }
+      fs.readdir(dataPath + '\\sd-screen\\', (err, files) => {
+        if (err) { return err } else { return files }
       })
     }
-  }
+  },
+  list
 }
 </script>
--- src/renderer/components/ScreenPage.vue
+++ src/renderer/components/ScreenPage.vue
@@ -1,12 +1,35 @@
 <template>
     <div>
-        SCREEN PAGE
+        <el-select v-model="value8" filterable placeholder="Screen Name" blur="getFiles">
+            <el-option
+                v-for="item in screenList"
+                :key="item"
+                :label="item"
+                :value="item.toString()">
+            </el-option>
+        </el-select>
     </div>
 </template>
 
 <script>
+import jsonFile from './JsonData/JsonFile'
 export default {
-  name: 'abc'
+  name: 'abc',
+  data () {
+    return {
+      value8: '',
+      screenList: ['a', 'b']
+    }
+  },
+  created () {
+    this.getFiles()
+  },
+  methods: {
+    getFiles: function () {
+      this.screenList = this.screenList.concat(jsonFile.list)
+      debugger
+    }
+  }
 }
 </script>
 
