15a01a4ea87193aab6da213f0f453dce9fdb54ff
Ryan Zou <ryanlemon@outlook.com>
2018-11-11 09:37:41 +0800

    Vue Material

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,5 +1,36 @@
 <template>
   <div id="app">
+    <md-app>
+      <md-app-toolbar class='md-primar' md-elevation='0'>
+        <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible">
+          <md-icon>menu</md-icon>
+        </md-button>
+        <span class="md-tilte">My Title</span>
+      </md-app-toolbar>
+      <md-app-drawer :md-active.sync="menuVisible" md-persistent="mini">
+        <md-toolbar class="md-transparent" md-elevation="0">
+          <span>Navigation</span>
+
+          <div class="md-toolbar-section-end">
+            <md-button class="md-icon-button md-dense" @click="toggleMenu">
+              <md-icon>keyboard_arrow_left</md-icon>
+            </md-button>
+          </div>
+        </md-toolbar> 
+        <md-list>
+          <md-list-item v-for="item in items"
+                        :key = item.index>
+            <router-link :to="item.name">
+              <md-icon>{{item.icon}}</md-icon>
+              <span class='md-list-item-text'>{{item.label}}</span>
+            </router-link>
+          </md-list-item>
+        </md-list>
+      </md-app-drawer>
+      <md-app-content>
+        <router-view></router-view>
+      </md-app-content>
+    </md-app>
     <el-container>
       <el-aside width="100px">
         <el-menu
@@ -26,7 +57,7 @@
     data () {
       return {
         items: [
-          {index: 1, name: 'landing', label: 'Landing'},
+          {index: 1, name: 'landing', label: 'Landing', icon: 'move_to_inbox'},
           {index: 2, name: 'screen', label: 'Screen'},
           {index: 3, name: 'translation', label: 'Translation'},
           {index: 4, name: 'message', label: 'Message'},
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import axios from 'axios'
-import ElementUI from 'element-ui'
+import VueMaterial from 'vue-material'
+import 'vue-material/dist/vue-material.min.css'
 
 import App from './App'
 import router from './router'
 import store from './store'
-import 'element-ui/lib/theme-chalk/index.css'
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
+Vue.use(VueMaterial)
 Vue.http = Vue.prototype.$http = axios
 Vue.config.productionTip = false
-Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   components: { App },
